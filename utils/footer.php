785c8b16ec1a994b2836422ffd746a2a3e71ff56
Gopinath Dehury <gopinath90biku@gmail.com>
2024-03-30 14:39:41 +0530

    first commit

---
+++ utils/footer.php
@@ -0,0 +1,38 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>C</strong><span class = "small footerSubtext">enturion</span>
+                        <strong>U</strong><span class = "small footerSubtext">niversity of</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology and</span>
+                        <strong>M</strong><span class = "small footerSubtext">anagement</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Jatani,Bhubaneswar,Khurda,Odisha
+                        &copy; Urban Events Venue & Photography, 2024.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    Gopinath Dehury  Multimedia Design/Web Engineer<br>
+                    Creative Computing 2022-2024.<br>
+                    Ahutosh,768108
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
